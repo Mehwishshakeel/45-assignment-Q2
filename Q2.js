b3edc315080be6e653d0f9e25cd2455d142799e5
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-03-05 13:43:51 +0500

    Add files via upload

---
+++ Q2.js
@@ -0,0 +1,4 @@
+"use strict";
+// QUESTION 2
+let personName = "Sara Naz";
+console.log("hello", personName, "would you like to learn python today?");
---
+++ Q2.ts
@@ -0,0 +1,3 @@
+// QUESTION 2
+let personName:string = "Sara Naz"
+console.log("hello",personName , "would you like to learn python today?")
